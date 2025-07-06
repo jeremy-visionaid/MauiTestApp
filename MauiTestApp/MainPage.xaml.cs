0e8761ca8fae0b89c65d599d4465a5e1a48b7673
Jeremy Powell <jeremy@visionaid.com>
2025-07-07 11:08:53 +1200

    Add callback sample

--- MauiTestApp/MainPage.xaml.cs
+++ MauiTestApp/MainPage.xaml.cs
@@ -1,25 +1,84 @@
-﻿namespace MauiTestApp
+﻿using System.Diagnostics;
+using AVFoundation;
+using CoreFoundation;
+using CoreMedia;
+
+namespace MauiTestApp;
+
+sealed class VideoCaptureDelegate : AVCaptureVideoDataOutputSampleBufferDelegate
 {
-    public partial class MainPage : ContentPage
+    public override void DidOutputSampleBuffer(AVCaptureOutput captureOutput, CMSampleBuffer sampleBuffer, AVCaptureConnection connection)
     {
-        int count = 0;
+        Debugger.Break();
+    }
+}
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+public partial class MainPage : ContentPage
+{
+	AVCaptureSession? captureSession;
+	VideoCaptureDelegate? videoCaptureDelegate;
+	AVCaptureVideoDataOutput? videoDataOutput;
+    int count = 0;
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+    public MainPage()
+    {
+        InitializeComponent();
     }
 
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        captureSession = new AVCaptureSession();
+        captureSession.BeginConfiguration();
+        videoCaptureDelegate = new VideoCaptureDelegate();
+
+        var videoDevice = AVCaptureDevice.GetDefaultDevice(AVMediaTypes.Video);
+        if (videoDevice == null)
+        {
+            Debug.WriteLine("No video device found");
+            return;
+        }
+
+        var videoInput = AVCaptureDeviceInput.FromDevice(videoDevice);
+        if (videoInput == null)
+        {
+            Debug.WriteLine("Failed to create video input");
+            return;
+        }
+
+        if (!captureSession.CanAddInput(videoInput))
+        {
+            Debug.WriteLine("Cannot add video input to capture session");
+            return;
+        }
+
+        captureSession.AddInput(videoInput);
+
+        videoDataOutput = new AVCaptureVideoDataOutput();
+        videoDataOutput.AlwaysDiscardsLateVideoFrames = true;
+        videoDataOutput.SetSampleBufferDelegate(videoCaptureDelegate, DispatchQueue.MainQueue);
+
+        if (!captureSession.CanAddOutput(videoDataOutput))
+        {
+            Debug.WriteLine("Failed to add video output");
+            return;
+        }
+
+        captureSession.AddOutput(videoDataOutput);
+
+        captureSession.CommitConfiguration();
+        captureSession.StartRunning();
+        Debug.WriteLine("Video capture started");
+    }
 }
