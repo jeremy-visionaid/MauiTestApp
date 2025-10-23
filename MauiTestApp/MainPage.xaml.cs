451cad4fe53e86956c63c9bb5c9b5d50e92f060f
Jeremy Powell <jeremy@visionaid.com>
2025-10-23 13:23:53 +1300

    Test UnhandledException event

--- MauiTestApp/MainPage.xaml.cs
+++ MauiTestApp/MainPage.xaml.cs
@@ -1,25 +1,24 @@
-﻿namespace MauiTestApp
+﻿using System.Diagnostics;
+
+namespace MauiTestApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Console.WriteLine($"Unhandled exception event");
+                var ex = args.ExceptionObject as Exception;
+            };
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Console.WriteLine($"Throwing exception");
+            throw new InvalidOperationException("Test exception.");
         }
     }
-
 }
