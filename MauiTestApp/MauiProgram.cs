776df9e3942b7f7493b897207aaa700782fa5d38
Jeremy Powell <jeremy@visionaid.com>
2025-10-29 13:58:47 +1300

    Add FontImageSource glyph test

--- MauiTestApp/MauiProgram.cs
+++ MauiTestApp/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace MauiTestApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("SegMDL2.ttf", "Segoe MDL2 Assets");
                 });
 
 #if DEBUG
