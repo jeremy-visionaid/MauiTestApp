8458e89399fd5b6c8c39f752cbf41476cb62410e
Jeremy Powell <jeremy@visionaid.com>
2026-03-04 10:37:21 +1300

    Reformat

--- MauiTestApp/AppShell.xaml.cs
+++ MauiTestApp/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace MauiTestApp
+﻿namespace MauiTestApp;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
